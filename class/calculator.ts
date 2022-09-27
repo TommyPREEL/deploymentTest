2ec60e254709fb4170d421ac7afd5d40ac961976
MathiasGenibrel <mathias.geni@gmail.com>
2022-09-27 11:35:09 +0200

    🎉 Test Calculator LiveCampus
    Learn to test an simple calculator

---
+++ class/calculator.ts
@@ -0,0 +1,41 @@
+class calculator {
+  /**
+   * Add two numbers
+   * @param a {number} - The first number
+   * @param b {number} - The second number
+   * @returns result of addition
+   */
+  public static add(a: number, b: number) {
+    return a + b;
+  }
+
+  /**
+   * Subtract two numbers
+   * @param a {number} - The first number
+   * @param b {number} - The second number
+   * @returns result of subtraction
+   */
+  public static sub(a: number, b: number) {
+    return a - b;
+  }
+
+  /**
+   * Multiply two numbers
+   * @param a {number} - The first number
+   * @param b {number} - The second number
+   * @returns result of multiplication
+   */
+  public static mul(a: number, b: number) {
+    return a * b;
+  }
+
+  /**
+   * Divide two numbers
+   * @param a {number} - The first number
+   * @param b {number} - The second number
+   * @returns result of division
+   */
+  public static div(a: number, b: number) {
+    return a / b;
+  }
+}
