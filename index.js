0b1d3c2119029096cb6f3f0a1d23d7f46d65fc17
tommypreellc <preel.tommy@gmail.com>
2023-05-02 16:11:30 +0200

    serv

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ var calculator_1 = require("./class/calculator");
 console.log(calculator_1.Calculator.add(1, 1));
 console.log(calculator_1.Calculator.sub(1, 1));
 console.log(calculator_1.Calculator.mul(5, 2));
+var http = require('http');
+var server = http.createServer(function (req, res) {
+    res.end('Voilà la réponse du serveur !');
+});
+server.listen(process.env.PORT || 3000);
--- index.ts
+++ index.ts
@@ -3,3 +3,11 @@ import { Calculator } from "./class/calculator";
 console.log(Calculator.add(1, 1));
 console.log(Calculator.sub(1, 1));
 console.log(Calculator.mul(5, 2));
+
+const http = require('http');
+
+const server = http.createServer((req:any, res:any) => {
+    res.end('Voilà la réponse du serveur !');
+});
+
+server.listen(process.env.PORT || 3000);
